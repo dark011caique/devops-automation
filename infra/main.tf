febe647b0e284a265d36ebe3a37697aacdd5de5d
dark011caique <caique.quiky@gmail.com>
2025-12-22 22:40:32 +0000

    versão do ubuntu

--- infra/main.tf
+++ infra/main.tf
@@ -21,17 +21,13 @@ resource "azurerm_subnet" "subnet" {
   address_prefixes     = ["10.0.1.0/24"]
 }
 
-
 resource "azurerm_public_ip" "public_ip" {
   name                = "public-ip-vm"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-
-  allocation_method   = "Static"   # Standard suporta Static e é o mais comum
-  sku                 = "Standard" # <- ajuste aqui
-  ip_version          = "IPv4"
+  allocation_method   = "Static"
+  sku                 = "Standard"
 }
-
 
 
 resource "azurerm_network_interface" "nic" {
