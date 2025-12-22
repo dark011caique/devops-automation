00b2828c55695cc37718f1004cd5a3b8bd6870e0
dark011caique <caique.quiky@gmail.com>
2025-12-22 15:12:58 +0000

    Ajuste senha

--- infra/variables.tf
+++ infra/variables.tf
@@ -14,5 +14,5 @@ variable "admin_password" {
   description = "Senha do usuário administrador"
   type        = string
   sensitive   = true
-  default     = "/"
+  default     = "Torresmo!@123!(*@)"
 }
